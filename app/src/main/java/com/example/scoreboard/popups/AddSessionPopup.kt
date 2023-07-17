1b5c45f0230f94bdff4f7f88a628e41a2b7dc69b
ozarskiw <ozarskiw@gmail.com>
2023-07-17 10:57:59 +0200

    Persistent tag selection visual in add popup

--- app/src/main/java/com/example/scoreboard/popups/AddSessionPopup.kt
+++ app/src/main/java/com/example/scoreboard/popups/AddSessionPopup.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -22,7 +23,9 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -59,7 +62,7 @@ class AddSessionPopup(val context: Context) : ComponentActivity() {
         ) {
             Column(
                 modifier = Modifier
-                    .width(280.dp)
+                    .widthIn(max = 450.dp, min = 0.dp)
                     .background(Color.LightGray, RoundedCornerShape(16.dp)),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -132,27 +135,31 @@ class AddSessionPopup(val context: Context) : ComponentActivity() {
                 .padding(top = 10.dp, bottom = 10.dp)
         ) {
             items(tagList.size) { index ->
-                TagPickListItem(tagList[index])
+                val tagPicked = remember { mutableStateOf(tagList[index].right) }
+                val tag = tagList[index].left
+                TagPickListItem(tag, tagPicked, tagList[index])
             }
         }
     }
 
     @Composable
-    fun TagPickListItem(item: MutablePair<Tag, Boolean>) {
+    fun TagPickListItem(tag: Tag, tagPicked: MutableState<Boolean>, item: MutablePair<Tag, Boolean>) {
         val textColor = remember { mutableStateOf(Color.Black) }
+        if(tagPicked.value) {
+            textColor.value = Color.Green
+        }
+        else{
+            textColor.value = Color.Black
+        }
         Text(
-            text = item.left.tagName,
+            text = tag.tagName,
             fontSize = 20.sp,
             color = textColor.value,
             modifier = Modifier
-                .padding(top = 6.dp)
+                .padding(vertical = 3.dp, horizontal = 10.dp)
                 .clickable {
-                    item.right = !item.right
-                    if (textColor.value == Color.Black) {
-                        textColor.value = Color.Green
-                    } else {
-                        textColor.value = Color.Black
-                    }
+                    tagPicked.value = !tagPicked.value
+                    item.right = tagPicked.value
                 },
         )
     }
@@ -209,4 +216,11 @@ class AddSessionPopup(val context: Context) : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun AddSessionPopupPreview() {
+    val popupVisible = remember { mutableStateOf(true) }
+    AddSessionPopup(context = LocalContext.current).GeneratePopup(popupVisible)
 }
