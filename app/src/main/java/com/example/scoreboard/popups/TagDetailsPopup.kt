94e3f5568ca6666f4bd593285df9d8d52ba15f55
ozarskiw <ozarskiw@gmail.com>
2023-07-18 12:22:48 +0200

    Tag details popup rough rework

--- app/src/main/java/com/example/scoreboard/popups/TagDetailsPopup.kt
+++ app/src/main/java/com/example/scoreboard/popups/TagDetailsPopup.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,10 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import com.example.scoreboard.MainActivity
 import com.example.scoreboard.Tag
 import com.example.scoreboard.database.TagDBService
@@ -36,6 +40,7 @@ class TagDetailsPopup(val context: Context, val tag: Tag) : ComponentActivity() 
         Popup(
             popupPositionProvider = WindowCenterOffsetPositionProvider(),
             onDismissRequest = { popupVisible.value = false },
+            properties = PopupProperties(focusable = true)
         ) {
             TagDetailsPopupLayout(popupVisible)
         }
@@ -45,78 +50,110 @@ class TagDetailsPopup(val context: Context, val tag: Tag) : ComponentActivity() 
     fun TagDetailsPopupLayout(popupVisible: MutableState<Boolean>) {
         Column(
             modifier = Modifier
-                .width(300.dp)
-                .height(300.dp)
+                .widthIn(min = 0.200.dp, max = 300.dp)
                 .background(Color.LightGray, RoundedCornerShape(16.dp)),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
+            verticalArrangement = Arrangement.Top
         ) {
-            TagName()
-            ChangeNameButton()
-            DeleteButton(popupVisible)
+            TagHeader()
+            val buttonsModifier = Modifier
+                .padding(horizontal = 10.dp)
+                .widthIn(100.dp)
+            Row(
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ChangeNameButton(buttonsModifier)
+                DeleteButton(popupVisible, buttonsModifier)
+            }
         }
     }
 
     @Composable
-    fun TagName() {
-        Text(text = "Tag name", fontSize = 25.sp, modifier = Modifier.padding(10.dp))
-        Text(text = tag.tagName, fontSize = 20.sp, modifier = Modifier.padding(bottom = 10.dp))
+    fun TagHeader() {
+        Text(text = "Tag details", fontSize = 15.sp, modifier = Modifier.padding(top = 10.dp))
+        Text(
+            text = tag.tagName,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(top = 10.dp, start = 10.dp, end = 10.dp),
+            textAlign = TextAlign.Center
+        )
     }
 
     @Composable
-    fun ChangeNameButton() {
+    fun ChangeNameButton(modifier: Modifier) {
         val dialogOpen = remember { mutableStateOf(false) }
         val newTagName = remember { mutableStateOf(tag.tagName) }
-        Button(onClick = { dialogOpen.value = true}, modifier = Modifier.padding(bottom = 10.dp)) {
-            Text(text = "Change name")
+        Button(
+            onClick = { dialogOpen.value = true }, modifier = modifier,
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            elevation = ButtonDefaults.elevation(0.dp)
+        ) {
+            Text(text = "Rename")
         }
-        if(dialogOpen.value){
-            Dialog(onDismissRequest = { dialogOpen.value = false }){
-                Column(
+        if (dialogOpen.value) {
+            ChangeNameDialog(dialogOpen, newTagName)
+        }
+    }
+
+    @Composable
+    fun ChangeNameDialog(dialogOpen: MutableState<Boolean>, newTagName: MutableState<String>) {
+        Dialog(onDismissRequest = { dialogOpen.value = false }) {
+            Column(
+                modifier = Modifier
+                    .width(300.dp)
+                    .background(Color.LightGray, RoundedCornerShape(16.dp))
+                    .padding(top = 10.dp, bottom = 10.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Change name",
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(10.dp)
+                )
+                OutlinedTextField(
+                    value = newTagName.value,
+                    onValueChange = { newTagName.value = it },
+                    label = { Text(text = "Tag name") },
                     modifier = Modifier
-                        .width(300.dp)
-                        .background(Color.LightGray, RoundedCornerShape(16.dp))
-                        .padding(10.dp),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    singleLine = true
+                )
+                Button(
+                    onClick = {
+                        if (newTagName.value != "") {
+                            tag.tagName = newTagName.value
+                            TagDBService(context).updateTag(tag)
+                        }
+                        dialogOpen.value = false
+                    },
+                    modifier = Modifier.padding(10.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                    elevation = ButtonDefaults.elevation(0.dp)
                 ) {
-                    Text(
-                        text = "Change name",
-                        fontSize = 25.sp,
-                        modifier = Modifier.padding(10.dp)
-                    )
-                    OutlinedTextField(
-                        value = newTagName.value,
-                        onValueChange = { newTagName.value = it },
-                        label = { Text(text = "Tag name") },
-                        modifier = Modifier
-                            .padding(10.dp)
-                            .fillMaxWidth()
-                    )
-                    Button(
-                        onClick = {
-                            if (newTagName.value != "") {
-                                tag.tagName = newTagName.value
-                                TagDBService(context).updateTag(tag)
-                            }
-                            dialogOpen.value = false
-                        },
-                        modifier = Modifier.padding(10.dp)
-                    ) {
-                        Text(text = "Change name")
-                    }
+                    Text(text = "Change name")
                 }
             }
         }
     }
 
     @Composable
-    fun DeleteButton(popupVisible: MutableState<Boolean>) {
-        Button(onClick = {
-            TagDBService(context).deleteTagByID(tag.id)
-            popupVisible.value = false
-            MainActivity.activitiesDataUpdate.value = true
-        }, modifier = Modifier.padding(bottom = 10.dp)) {
+    fun DeleteButton(popupVisible: MutableState<Boolean>, modifier: Modifier) {
+        Button(
+            onClick = {
+                TagDBService(context).deleteTagByID(tag.id)
+                popupVisible.value = false
+                MainActivity.activitiesDataUpdate.value = true
+            },
+            modifier = modifier,
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            elevation = ButtonDefaults.elevation(0.dp)
+        ) {
             Text(text = "Delete")
         }
     }
