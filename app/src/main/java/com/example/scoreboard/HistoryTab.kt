e05c41351f658d78549e911d1dd96f13ed736caf
ozarskiw <ozarskiw@gmail.com>
2023-07-28 10:24:40 +0200

    Added session filter button and duration text in history tab.

--- app/src/main/java/com/example/scoreboard/HistoryTab.kt
+++ app/src/main/java/com/example/scoreboard/HistoryTab.kt
@@ -9,23 +9,32 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.scoreboard.database.SessionDBService
+import com.example.scoreboard.popups.FilterHistoryPopup
 import com.example.scoreboard.popups.SessionDetailsPopup
 import com.example.scoreboard.session.Session
+import org.apache.commons.lang3.tuple.MutablePair
 
 class HistoryTab(val context: Context) : ComponentActivity() {
 
@@ -35,7 +44,7 @@ class HistoryTab(val context: Context) : ComponentActivity() {
 
     @Composable
     fun GenerateLayout() {
-        sessions = remember { SnapshotStateList() }
+        sessions = remember { mutableStateListOf() }
         sessionDetailsPopupVisible = remember { mutableStateOf(false) }
         var tempSessions = SessionDBService(context).getAllSessions()
         tempSessions = tempSessions.sortedByDescending { it.getDate().timeInMillis }
@@ -57,7 +66,48 @@ class HistoryTab(val context: Context) : ComponentActivity() {
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Top
         ) {
+            FilterSessionsByTagsRow()
             SessionsHistoryList()
+        }
+    }
+
+    @Composable
+    fun FilterSessionsByTagsRow() {
+        val filterPopupVisible = remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            var selectedSessionsDuration = 0L
+            sessions.forEach { selectedSessionsDuration += it.getDuration() }
+            val selectedSessionsDurationString =
+                durationInSecondsToHoursAndMinutes(selectedSessionsDuration)
+            Text(
+                text = String.format(
+                    context.getString(R.string.selected_sessions_duration),
+                    selectedSessionsDurationString
+                ),
+                fontSize = 20.sp,
+                modifier = Modifier.padding(start = 10.dp)
+            )
+            Button(
+                onClick = {
+                    filterPopupVisible.value = !filterPopupVisible.value
+                },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                elevation = ButtonDefaults.elevation(0.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Settings,
+                    contentDescription = "Filter popup button",
+                    tint = Color.Black,
+                    modifier = Modifier.size(25.dp)
+                )
+            }
+        }
+        if (filterPopupVisible.value) {
+            FilterHistoryPopup(context).GeneratePopup(filterPopupVisible, sessions)
         }
     }
 
