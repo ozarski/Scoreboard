8a7fa1f5d4da9589d3e436221610e0e4cd4d4704
ozarskiw <ozarskiw@gmail.com>
2024-02-08 16:26:15 +0100

    Created Versions and Dependencies objects

---
+++ buildSrc/Versions.kt
@@ -0,0 +1,12 @@
+object Versions{
+    const val androidx_core_ktx =  "1.12.0"
+    const val androidx_lifecycle_runtime = "2.7.0"
+    const val androidx_activity_compose = "1.8.2"
+    const val androidx_compose_bom = "2022.10.00"
+    const val google_accompanist_pager = "0.25.0"
+    const val chargemap_compose_numberpicker = "1.0.3"
+    const val apache_commons_lang3 = "3.12.0"
+    const val junit = "4.13.2"
+    const val androidx_test_ext_junit = "1.1.5"
+    const val androidx_test_espresso_core = "3.5.1"
+}
