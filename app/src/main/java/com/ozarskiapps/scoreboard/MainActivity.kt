0f6efc836ff5a3e607bb23d195e129fa4ccc0ca2
ozarskiw <ozarskiw@gmail.com>
2024-02-28 13:29:10 +0100

    Removed accompanist dependencies and replaced UI elements with material 3

--- app/src/main/java/com/ozarskiapps/scoreboard/MainActivity.kt
+++ app/src/main/java/com/ozarskiapps/scoreboard/MainActivity.kt
@@ -3,35 +3,36 @@ package com.ozarskiapps.scoreboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.TabPosition
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabPosition
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.UiComposable
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.PagerState
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import com.google.accompanist.pager.rememberPagerState
 import com.ozarskiapps.scoreboard.ui.ActivitiesTab
 import com.ozarskiapps.scoreboard.ui.HistoryTab
 import com.ozarskiapps.scoreboard.ui.theme.ScoreboardTheme
@@ -39,12 +40,9 @@ import com.ozarskiapps.scoreboard.ui.theme.Typography
 import com.ozarskiapps.scoreboard.ui.theme.backgroundDark
 import com.ozarskiapps.scoreboard.ui.theme.onPrimaryDark
 import com.ozarskiapps.scoreboard.ui.theme.primaryDark
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -58,7 +56,7 @@ class MainActivity : ComponentActivity() {
     }
 
 
-    @ExperimentalPagerApi
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     fun LayoutMain() {
 
@@ -66,9 +64,22 @@ class MainActivity : ComponentActivity() {
             this.getString(R.string.main_tabs_activity_tab_title),
             this.getString(R.string.main_tabs_history_tab_title)
         )
-        val tabIndex = remember { mutableIntStateOf(0) }
-        val pagerState = rememberPagerState()
-        val scope = rememberCoroutineScope()
+        val selectedTabIndex = remember {
+            mutableIntStateOf(0)
+        }
+        val pagerState = rememberPagerState {
+            tabs.size
+        }
+
+        LaunchedEffect(selectedTabIndex.intValue) {
+            pagerState.animateScrollToPage(selectedTabIndex.intValue)
+        }
+
+        LaunchedEffect(pagerState.currentPage, pagerState.isScrollInProgress) {
+            if (!pagerState.isScrollInProgress) {
+                selectedTabIndex.intValue = pagerState.currentPage
+            }
+        }
 
         Column(
             modifier = Modifier
@@ -76,74 +87,71 @@ class MainActivity : ComponentActivity() {
                 .background(color = backgroundDark),
             verticalArrangement = Arrangement.Bottom
         ) {
-            MainTabsPager(pagerState = pagerState, tabs = tabs, modifier = Modifier.weight(1f))
+            MainTabsPager(
+                pagerState = pagerState,
+                tabs = tabs,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
 
-            Row(modifier = Modifier.padding(horizontal = 5.dp)) {
-                TabRow(
-                    selectedTabIndex = tabIndex.intValue,
-                    indicator = tabIndicator(pagerState = pagerState),
-                    backgroundColor = onPrimaryDark,
-                    modifier = Modifier.clip(
-                        shape = RoundedCornerShape(
-                            topStart = 25.dp,
-                            topEnd = 25.dp
-                        )
+            TabRow(
+                selectedTabIndex = selectedTabIndex.intValue,
+                indicator = tabIndicator(selectedTabIndex),
+                containerColor = onPrimaryDark,
+                modifier = Modifier.clip(
+                    shape = RoundedCornerShape(
+                        topStart = 25.dp,
+                        topEnd = 25.dp
                     )
-                ) {
-                    MainTabs(tabIndex, scope, pagerState, tabs)
-                }
+                )
+            ) {
+                MainTabs(selectedTabIndex, tabs)
             }
         }
     }
 
-    @OptIn(ExperimentalPagerApi::class)
     @Composable
-    private fun tabIndicator(pagerState: PagerState): @Composable @UiComposable (List<TabPosition>) -> Unit {
+    private fun tabIndicator(selectedTabIndex: MutableState<Int>): @Composable @UiComposable (List<TabPosition>) -> Unit {
         val indicator: @Composable @UiComposable
             (tabPositions: List<TabPosition>) -> Unit = @Composable { tabPositions ->
-            TabRowDefaults.Indicator(
+            SecondaryIndicator(
                 Modifier
-                    .pagerTabIndicatorOffset(pagerState, tabPositions)
+                    .tabIndicatorOffset(tabPositions[selectedTabIndex.value])
                     .height(3.dp)
                     .padding(horizontal = 60.dp)
                     .offset(y = ((-6).dp))
                     .clip(shape = RoundedCornerShape(16.dp)),
-                color = primaryDark,
+                color = primaryDark
             )
         }
         return indicator
     }
 
 
-    @ExperimentalPagerApi
     @Composable
     fun MainTabs(
-        tabIndex: MutableState<Int>,
-        scope: CoroutineScope,
-        pagerState: PagerState,
+        selectedTabIndex: MutableState<Int>,
         tabs: List<String>
     ) {
         tabs.forEachIndexed { index, title ->
             Tab(
                 text = { Text(title, style = Typography.titleLarge, color = primaryDark) },
-                selected = tabIndex.value == index,
+                selected = selectedTabIndex.value == index,
                 onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                    tabIndex.value = index
+                    selectedTabIndex.value = index
                 }
             )
         }
     }
 
-    @ExperimentalPagerApi
+    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     fun MainTabsPager(pagerState: PagerState, tabs: List<String>, modifier: Modifier) {
         HorizontalPager(
             state = pagerState,
-            count = tabs.size,
-            modifier = modifier
+            modifier = modifier,
+            beyondBoundsPageCount = 1
         ) {
             when (it) {
                 0 -> {
